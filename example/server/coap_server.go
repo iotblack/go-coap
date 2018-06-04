594fd8fdd09e7ce5d7f3d701819a5ca414f4848c
huxinbang <huxinbang@gmail.com>
2018-06-04 15:36:41 +0800

    add rpc

--- example/server/coap_server.go
+++ example/server/coap_server.go
@@ -7,37 +7,55 @@ import (
 	"github.com/dustin/go-coap"
 )
 
-func handleA(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
-	log.Printf("Got message in handleA: path=%q: %#v from %v", m.Path(), m, a)
+func handleAuth(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
+	log.Printf("Got message in handleAuth: path=%q: %#v from %v", m.Path(), string(m.Payload), a)
 	if m.IsConfirmable() {
 		res := &coap.Message{
 			Type:      coap.Acknowledgement,
-			Code:      coap.Content,
+			Code:      coap.Created,
 			MessageID: m.MessageID,
 			Token:     m.Token,
-			Payload:   []byte("hello to you!"),
+			Payload:   []byte("Token for Coap"),
 		}
 		res.SetOption(coap.ContentFormat, coap.TextPlain)
 
-		log.Printf("Transmitting from A %#v", res)
+		log.Printf("Transmitting from Auth %#v", string(res.Payload))
 		return res
 	}
 	return nil
 }
 
-func handleB(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
-	log.Printf("Got message in handleB: path=%q: %#v from %v", m.Path(), m, a)
+func handleMqttPub(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
+	log.Printf("Got message in handleMqttPub: path=%q: %#v from %v", m.Path(), string(m.Payload), a)
 	if m.IsConfirmable() {
 		res := &coap.Message{
 			Type:      coap.Acknowledgement,
-			Code:      coap.Content,
+			Code:      coap.Created,
 			MessageID: m.MessageID,
 			Token:     m.Token,
-			Payload:   []byte("good bye!"),
+			Payload:   []byte("Publish message success"),
 		}
 		res.SetOption(coap.ContentFormat, coap.TextPlain)
 
-		log.Printf("Transmitting from B %#v", res)
+		log.Printf("Transmitting from MqttPub %#v", string(res.Payload))
+		return res
+	}
+	return nil
+}
+
+func handleMqttRpc(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
+	log.Printf("Got message in handleMqttRpc: path=%q: %#v from %v", m.Path(), string(m.Payload), a)
+	if m.IsConfirmable() {
+		res := &coap.Message{
+			Type:      coap.Acknowledgement,
+			Code:      coap.Created,
+			MessageID: m.MessageID,
+			Token:     m.Token,
+			Payload:   []byte("Rpc Call success"),
+		}
+		res.SetOption(coap.ContentFormat, coap.TextPlain)
+
+		log.Printf("Transmitting from MqttRpc %#v", string(res.Payload))
 		return res
 	}
 	return nil
@@ -45,8 +63,9 @@ func handleB(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
 
 func main() {
 	mux := coap.NewServeMux()
-	mux.Handle("/a", coap.FuncHandler(handleA))
-	mux.Handle("/b", coap.FuncHandler(handleB))
+	mux.Handle("/auth", coap.FuncHandler(handleAuth))
+	mux.Handle("/mqttpub", coap.FuncHandler(handleMqttPub))
+	mux.Handle("/mqttrpc", coap.FuncHandler(handleMqttRpc))
 
-	log.Fatal(coap.ListenAndServe("udp", ":5683", mux))
+	log.Fatal(coap.ListenAndServe("udp", "127.0.0.1:5683", mux))
 }
